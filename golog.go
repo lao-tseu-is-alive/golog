a73969c7f019081b035cba289784afa297b51a77
cgil <lao.tseu.is.alive@gmail.com>
2019-06-12 17:42:51 +0200

    added support for format strings like in sprintf

--- golog.go
+++ golog.go
@@ -28,34 +28,34 @@ func addTimeStamp(message string) string {
 	return fmt.Sprintf("%s %s", timeString, message)
 }
 
-func Info(message string) {
+func Info(message string, v ...interface{}) {
 	blue := ansi.ColorFunc("cyan+")
-	err := loggerInfo.Output(2, blue(addTimeStamp(message)))
+	err := loggerInfo.Output(2, fmt.Sprintf(blue(addTimeStamp(message)), v...))
 	if err != nil {
-		log.Println("ERROR trying to output Info(message) to stderr console !")
+		log.Fatalln("ERROR trying to output Info(message) to stderr console !")
 	}
 }
 
-func Trace(message string) {
+func Trace(message string, v ...interface{}) {
 	magenta := ansi.ColorFunc("magenta+b")
-	err := loggerTrace.Output(2, magenta(addTimeStamp(message)))
+	err := loggerTrace.Output(2, fmt.Sprintf(magenta(addTimeStamp(message)), v...))
 	if err != nil {
-		log.Println("ERROR trying to output Trace(message) to stderr console !")
+		log.Fatalln("ERROR trying to output Trace(message) to stderr console !")
 	}
 }
 
-func Warn(message string) {
+func Warn(message string, v ...interface{}) {
 	yellow := ansi.ColorFunc("yellow+b")
-	err := loggerWarning.Output(2, yellow(addTimeStamp(message)))
+	err := loggerWarning.Output(2, fmt.Sprintf(yellow(addTimeStamp(message)), v...))
 	if err != nil {
-		log.Println("ERROR trying to output Warn(message) to stderr console !")
+		log.Fatalln("ERROR trying to output Warn(message) to stderr console !")
 	}
 }
 
-func Err(message string) {
+func Err(message string, v ...interface{}) {
 	red := ansi.ColorFunc("red+b:white+h")
-	err := loggerError.Output(2, red(addTimeStamp(message)))
+	err := loggerError.Output(2, fmt.Sprintf(red(addTimeStamp(message)), v...))
 	if err != nil {
-		log.Println("ERROR trying to output Err(message) to stderr console !")
+		log.Fatalln("ERROR trying to output Err(message) to stderr console !")
 	}
 }
